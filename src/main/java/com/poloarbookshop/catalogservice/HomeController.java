801bab899a97ea6303bfec05c0529eb8a11a1b82
Seine <>
2024-07-22 16:29:23 +0900

    20240722 CNCF

---
+++ src/main/java/com/poloarbookshop/catalogservice/HomeController.java
@@ -0,0 +1,12 @@
+package com.poloarbookshop.catalogservice;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return "도서 카탈로그에 오신 것을 환영합니다!";
+    }
+}
