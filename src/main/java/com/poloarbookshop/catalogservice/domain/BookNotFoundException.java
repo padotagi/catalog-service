f842d234afb92d43c96c06b0d63e9af7ce249b73
Seine <>
2024-07-22 17:24:29 +0900

    20240722 2

---
+++ src/main/java/com/poloarbookshop/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,8 @@
+package com.poloarbookshop.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+}
---
+++ src/main/java/com/poloarbookshop/catalogservice/web/BookController.java
@@ -0,0 +1,24 @@
+package com.poloarbookshop.catalogservice.web;
+
+
+import com.poloarbookshop.catalogservice.domain.Book;
+import com.poloarbookshop.catalogservice.domain.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public Iterable<Book> get() {
+        return bookService.viewBookList();
+    }
+}
