edd456227325798149928ea7b5331e0b3eea85bd
Seine <>
2024-08-05 15:24:38 +0900

    20240805 conf

--- src/main/java/com/poloarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/poloarbookshop/catalogservice/HomeController.java
@@ -1,12 +1,19 @@
 package com.poloarbookshop.catalogservice;
 
+import com.poloarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "도서 카탈로그에 오신 것을 환영합니다!";
+        return polarProperties.getGreeting();
     }
 }
